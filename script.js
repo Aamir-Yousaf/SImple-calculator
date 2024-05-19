e86bc3d06a512d330ac57f3fb3080b6344c44b25
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-05-19 09:54:58 +0500

    add js functionality evaluate

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let expSelector = document.querySelector('.exp');
+let resultSelector = document.querySelector('.result');
 
+
+let exp = "";
+function makeExp(char){
+  exp = exp + char;
+  console.log(exp);
+  expSelector.innerHTML = exp;
+
+}
+function makeResult(){
+   const result = eval(exp);
+   console.log(result);
+   resultSelector.innerHTML = result;
+}
+function clearResult(){
+  expSelector.innerHTML = "_";
+  resultSelector.innerHTML = "0";
+  exp="";
+}
